0fc18f84f3c1b773d49838b367a5c1cf1bc85c5f
Sergio Rodriguez Vieira <sergiorodriguezvieira@gmail.com>
2025-10-29 21:30:42 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ['El Perro', 'Mi abuela', 'El cartero', 'Mi pajaro', 'Mi gato'];
+
+let action = ['se comio', 'orino', 'rompio', 'destrozo', 'escupio'];
+
+let what = ['mi tarea', 'mi telefono', 'el carro', 'mi cara'];
+
+let when = ['antes de la clase.', 'cuando estaba durmiendo.', 'mientras ejercitaba.', 'durante el almuerzo.'];
+
+  window.onload = function () {
+    const whoElement = who[Math.floor(Math.random() * who.length)];
+    const actionElement = action[Math.floor(Math.random() * action.length)];
+    const whatElement = what[Math.floor(Math.random() * what.length)];
+    const whenElement = when[Math.floor(Math.random() * when.length)];
+
+    const excuse = whoElement + " " + actionElement + " " + whatElement + " " + whenElement;
+
+    document.getElementById("excuse").innerHTML = excuse
+
+
+  }
